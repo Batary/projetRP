e5089c1d8e903599892a3fafc6160d2ea3bc706b
zhell <thomas.zanivan@gmail.com>
2018-05-12 23:07:43 +0200

    fin output

--- Code/test-executable.sh
+++ Code/test-executable.sh
@@ -6,7 +6,10 @@ make
 # pour changer le repertoire de sortie : -out <dossier>
 ./steiner.exe -print -verbose -time 10 -file ../Instances/test/example.stp
 
+#output avec un seul fichier:
+./steiner.exe -print -verbose -time 10 -file ../Instances/D/d14.stp -out ../Output/test/ -gene
 
-# avec un dossier:
+
+# output avec un dossier:
 # ./steiner.exe -print -verbose -time 10 -dir ../Instances/B/ -out ../Output/B/ -local -pop 1 -verbose
 
